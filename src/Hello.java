ebaba392278e033863f114f32e1be4054ea3a9b2
code code <ozeldersbirebiritu@gmail.com>
2019-07-04 22:54:45 +0200

    fifth hello world in feature branch

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,4 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello fucking world");
-        System.out.println("Hello");
-        System.out.println("Hello");
     }
 }
